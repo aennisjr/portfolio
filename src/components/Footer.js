9f93b022c40b9bea94c87e6e00b215d736a0da07
aennisjr <aggreyennis@outlook.com>
2021-02-28 23:13:25 -0500

    Add Github project link to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -10,7 +10,7 @@ class Footer extends Component {
 			<div>
 				<div className="spacer-50"></div>
 				<div className="text-muted text-center mt-5 mb-3">
-					<small>Source code for this site available on <a href="#">GitHub</a></small> <FaGithub/>
+					<small>Source code for this site available on <a href="https://github.com/aennisjr/portfolio" rel="noreferrer" target="_blank">GitHub</a></small> <FaGithub/>
 				</div>
 			</div>
 		)
